bc37a316101d606d9bc64104188b0e6cd28da81f
Ryan Huang <huang@cs.jhu.edu>
2020-01-10 02:16:12 -0500

    dfs walking of def-use produces more readable output

--- analyzer/lib/DefUse/DefUse.h
+++ analyzer/lib/DefUse/DefUse.h
@@ -10,6 +10,7 @@
 #define __DEFUSE_H_
 
 #include <queue>
+#include <stack>
 #include <utility>
 #include <vector>
 
@@ -18,6 +19,7 @@
 #include "llvm/IR/Value.h"
 
 #include "llvm/ADT/GraphTraits.h"
+#include "llvm/ADT/DepthFirstIterator.h"
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/None.h"
 
@@ -25,6 +27,11 @@
 
 namespace llvm {
 namespace defuse {
+
+enum class UserGraphWalkType {
+  BFS,
+  DFS
+};
 
 // User graph for a given instruction: it includes the direct users as well as
 // the transitive closure of users' users etc.
@@ -36,76 +43,107 @@ class UserGraph {
    typedef UserNodeList::const_iterator const_iterator;
    typedef SmallPtrSet<const Value *, 8> VistedNodeSet;
    typedef std::queue<Optional<const Value *>> VisitQueue;
+   typedef std::stack<const Value *> VisitStack;
 
-   UserGraph(const Value *v, int depth = -1) {
+   UserGraph(const Value *v, UserGraphWalkType t=UserGraphWalkType::DFS, int depth=-1) {
      root = v;
-     queue.push(v);
-     queue.push(None);  // a dummy element marking end of a level
-     int level = 0;
-     while (!queue.empty()) {
-       Optional<const Value *> head = queue.front();
-       if (head != None) {
-         const Value *elem = head.getValue();
-         for (auto ui = elem->user_begin(); ui != elem->user_end(); ++ui) {
-           if (const Instruction *inst = dyn_cast<Instruction>(*ui)) {
-             if (const StoreInst *store = dyn_cast<StoreInst>(inst)) {
-               // if this is a store instruction, it does not have users. we
-               // need to find the users of the target (second operand) instead.
-               if (const Instruction *op =
-                       dyn_cast<Instruction>(store->getOperand(1))) {
-                 if (visited.insert(op).second) {
-                   queue.push(op);
-                 }
-               }
-             } else {
-               if (visited.insert(inst).second) {
-                 queue.push(inst);
-               }
-             }
-           }
-         }
-         // front is a regular element (User) that has not been visited before
-         // put it into the user list
-         if (level > 0) {
-           const Value *user = head.getValue();
-           if (!isa<AllocaInst>(user)) {
-             // only if this is not an alloca instruction, which comes from
-             // the getOperand handling of StoreInstruction.
-             userList.push_back(UserNode(user, level));
-           }
-         }
+     maxDepth = depth;
+     if (t == UserGraphWalkType::DFS)
+       doDFS();
+     else
+       doBFS();
+   }
+
+   const_iterator begin() const { return userList.begin(); }
+   const_iterator end() const { return userList.end(); }
+   iterator begin() { return userList.begin(); }
+   iterator end() { return userList.end(); }
+
+  protected:
+   void doDFS() {
+     visited.insert(root);
+     visit_stack.push(root);
+     while (!visit_stack.empty()) {
+       const Value *elem = visit_stack.top();
+       if (elem != root && !isa<AllocaInst>(elem)) {
+         // skip alloca inst, which comes from the operand of StoreInstruction.
+         userList.push_back(UserNode(elem, 1));
        }
-       queue.pop();  // remove the front element
-       if (!queue.empty()) {
-         head = queue.front();
-         if (head != None) {
-           continue;
-         }
-         // reached the marker, meaning we are about to go to the next level
-         // check the level depth here so we may stop
-         level += 1;
-         queue.pop();
-         if (depth > 0 && level > depth) {
-           break;
-         }
-         if (!queue.empty()) {
-           // if this is not the last, add a marker at the end of the queue
-           queue.push(None);
-         }
-       }
+       visit_stack.pop();
+       processUser(elem, UserGraphWalkType::DFS);
      }
     }
 
-    const_iterator begin() const { return userList.begin(); }
-    const_iterator end() const { return userList.end(); }
-    iterator begin() { return userList.begin(); }
-    iterator end() { return userList.end(); }
+    void doBFS() {
+      visited.insert(root);
+      visit_queue.push(root);
+      visit_queue.push(None);  // a dummy element marking end of a level
+      int level = 0;
+      while (!visit_queue.empty()) {
+        Optional<const Value *> head = visit_queue.front();
+        if (head != None) {
+          const Value *elem = head.getValue();
+          if (elem != root && !isa<AllocaInst>(elem)) {
+            // skip alloca inst, which comes from the operand of StoreInstruction.
+            userList.push_back(UserNode(elem, level));
+          }
+          processUser(elem, UserGraphWalkType::BFS);
+        }
+        visit_queue.pop();  // remove the front element
+        if (!visit_queue.empty()) {
+          head = visit_queue.front();
+          if (head != None) {
+            continue;
+          }
+          // reached the marker, meaning we are about to go to the next level
+          // check the level depth here so we may stop
+          level += 1;
+          visit_queue.pop();
+          if (maxDepth > 0 && level > maxDepth) {
+            break;
+          }
+          if (!visit_queue.empty()) {
+            // if this is not the last, add a marker at the end of the visit_queue
+            visit_queue.push(None);
+          }
+        }
+      }
+    }
 
-   private:
-    const Value* root;
-    UserNodeList userList;
-    VistedNodeSet visited;
-    VisitQueue queue;
+  private:
+   void processUser(const Value *elem, UserGraphWalkType walk) {
+     for (auto ui = elem->user_begin(); ui != elem->user_end(); ++ui) {
+       if (const Instruction *inst = dyn_cast<Instruction>(*ui)) {
+         if (const StoreInst *store = dyn_cast<StoreInst>(inst)) {
+           if (const Instruction *op = dyn_cast<Instruction>(store->getOperand(1))) {
+             // if this is a store instruction, it does not have users. we
+             // need to find the users of the target (second operand) instead.
+             if (visited.insert(op).second) {
+               if (walk == UserGraphWalkType::DFS)
+                 visit_stack.push(op);
+               else
+                 visit_queue.push(op);
+             }
+           }
+         } else {
+           if (visited.insert(inst).second) {
+             if (walk == UserGraphWalkType::DFS)
+               visit_stack.push(inst);
+             else
+               visit_queue.push(inst);
+           }
+         }
+       }
+     }
+   }
+
+  private:
+   int maxDepth;
+   const Value *root;
+   UserNodeList userList;
+   VistedNodeSet visited;
+   VisitQueue visit_queue;
+   VisitStack visit_stack;
 };
 
 } // namespace defuse
