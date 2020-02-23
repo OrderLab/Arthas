1dec7203c83002794a0414d94cbdab1edb3b35e3
portugasian <jisoochoi96@gmail.com>
2020-02-22 19:30:50 -0500

    testing with address print

--- test/simple.c
+++ test/simple.c
@@ -4,10 +4,13 @@ int main(){
    int a = 3;
    int b = 5;
    int c;
-   printf("a is %d\n", a);
+   int *d;
+   d = malloc(sizeof(int));
+   *d = 3;
    if(b == 5){
       a++;
    }
+   printf("d is %p\n", d);
    c = a + b;
    return 0;
 }
