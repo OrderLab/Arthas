6bd9a33d60f7c38ac748e35e39ed60a33538ec64
Ryan Huang <huang@cs.jhu.edu>
2020-02-24 02:05:22 -0500

    add runtime placeholder for more complex/efficient address runtime tracing func

---
+++ analyzer/runtime/addr_tracker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+FILE *__arthas_tracker_file;
+
+#define MAX_FILE_NAME_SIZE 128
+
+inline char *__arthas_tracker_file_name(char * buf)
+{
+  snprintf(buf, 128, "pmem_addr_pid_%d.dat", getpid()); 
+  return buf;
+}
+
+void __arthas_addr_tracker_init() 
+{
+  char filename_buf[MAX_FILE_NAME_SIZE];
+  char *filename = __arthas_tracker_file_name(filename_buf);
+  fprintf(stderr, "openning address tracker output file %s\n", filename);
+  __arthas_tracker_file = fopen(filename, "w");
+}
+
+inline void __arthas_track_addr(void *addr) 
+{
+  fprintf(__arthas_tracker_file, "%p,%s,%d\n", addr, __FILE__, __LINE__);
+}
+
+bool __arthas_addr_tracker_dump()
+{
+  return true;
+}
