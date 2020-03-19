7a27ddbe746b3f622286e2892c77d23f40078086
Ryan Huang <huang@cs.jhu.edu>
2020-03-19 15:14:23 -0400

    add dg test cases for testing pointer analysis

---
+++ test/dg/funcptr1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+
+void set(int *a)
+{
+	*a = 8;
+}
+
+/* basic test */
+int main(void)
+{
+	void (*s)(int *) = set;
+	int a, b = 1, c = 13;
+	a = b + c;
+	b = 3;
+	c = 5;
+	s(&a);
+  printf("a = %d\n", a);
+	assert(a == 8);
+	return 0;
+}
