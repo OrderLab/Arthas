59e03d6a93a9f76141927dce23a504b1869c73f5
portugasian <jisoochoi96@gmail.com>
2021-03-06 16:40:36 -0500

    working with 10 secs

--- scripts/memcached_refcount_reexec.sh
+++ scripts/memcached_refcount_reexec.sh
@@ -3,7 +3,7 @@
 scripts_dir=$(cd "$(dirname "${BASH_SOURCE-$0}")"; pwd)
 
 (sleep 1 ; timeout 8 $scripts_dir/memcached_refcount_twoclients.sh) & X=$!
-(sleep 2 ; ) & Y=$!
+(sleep 2 ; $scripts_dir/killScript) & Y=$!
 ($scripts_dir/memcached-refcount -p 11211 -U 11211) & 
 echo $! > memcached.pid
 wait $X
